59795adb1ae4bc2cee7b15e368ad28f85e5dcb08
shreyans-gits <shreyans.sahu07@gmail.com>
2026-05-29 15:54:12 +0530

    fixed the camera flip issue

--- nova_mobile/main.py
+++ nova_mobile/main.py
@@ -710,7 +710,7 @@ class NovaMobileApp(MDApp):
                 break
                 
             try:
-                frame = cv2.flip(frame, 0)
+                frame = cv2.flip(frame, 1)
                 _, encoded_img = cv2.imencode('.jpg', frame, [int(cv2.IMWRITE_JPEG_QUALITY), 65])
                 raw_bytes = encoded_img.tobytes()
                 
